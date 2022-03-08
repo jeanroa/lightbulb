c9a92a944499c0f984aeffea01a89a9a27ec798c
jeanroa <roajeanmarco@gmail.com>
2022-03-08 00:10:15 -0300

    Se crea la funcionalidad, faltan estilos

---
+++ main.js
@@ -0,0 +1,18 @@
+
+function changeColorBlue() {
+  var element = document.getElementById("lightbulb")
+  element.classList.add("lightbulb-blue")
+  element.classList.remove("lightbulb-red", "lightbulb-green")
+}
+
+function changeColorRed() {
+  var element = document.getElementById("lightbulb")
+  element.classList.add("lightbulb-red")
+  element.classList.remove("lightbulb-blue", "lightbulb-green")
+}
+
+function changeColorGreen() {
+  var element = document.getElementById("lightbulb")
+  element.classList.add("lightbulb-green")
+  element.classList.remove("lightbulb-red", "lightbulb-blue")
+}
