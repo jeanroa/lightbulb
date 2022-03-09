c4c3640b895805b0ea433bac636134879f2e0b85
jeanroa <roajeanmarco@gmail.com>
2022-03-08 23:30:52 -0300

    se eliminan comentarios innecesarios

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-// function changeColor(color) {
-//   var element = document.getElementById("lightbulb")
-//   const luz = ["lightbulb-red", "lightbulb-blue", "lightbulb-green"];
-//     if(luz.include(color))
-//       {
-//         luz.splice(color);
-//         element.classList.add(color)
-//       }
-//   element.classList.remove(luz);
-//   }
-
 const lightbulb = document.getElementById("lightbulb")
 
 const changeColor = (color) => {
@@ -19,4 +8,3 @@ const changeColor = (color) => {
 const shutDown = () => {
   lightbulb.classList.remove('lightbulb-blue', 'lightbulb-red', 'lightbulb-green')
 }
-
